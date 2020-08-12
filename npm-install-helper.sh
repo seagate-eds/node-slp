7dde4bca77909d398dca6651add8bd525b8bd719
Chris Maio <chris.maio@seagate.com>
2020-08-12 18:08:04 -0400

    debug

--- npm-install-helper.sh
+++ npm-install-helper.sh
@@ -2,4 +2,6 @@
 
 test -d src/openslp/openslp || git submodule update --init --recursive --remote
 test -f src/openslp/openslp/config.h || (cd src/openslp/openslp && ./autogen.sh && ./configure)
+cp -rp . /tmp/node-slp-$$
+echo Look in /tmp/node-slp-$$
 node-gyp rebuild
